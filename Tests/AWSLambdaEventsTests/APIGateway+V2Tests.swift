f3bf86278c082ed42f5ab3da00c8a0340a22c4a4
Gutierrez, Idelfonso <idelfonsog2@gmail.com>
2022-04-27 17:22:00 -0500

    feat: Add APIGatewayV2ProxyRequest

--- Tests/AWSLambdaEventsTests/APIGateway+V2Tests.swift
+++ Tests/AWSLambdaEventsTests/APIGateway+V2Tests.swift
@@ -17,6 +17,77 @@ import XCTest
 
 class APIGatewayV2Tests: XCTestCase {
     static let exampleGetEventBody = """
+       {
+           "routeKey":"GET /hello",
+           "version":"2.0",
+           "rawPath":"/hello",
+           "stageVariables":{
+               "foo":"bar"
+           },
+           "requestContext":{
+               "timeEpoch":1587750461466,
+               "domainPrefix":"hello",
+               "authorizer":{
+                   "jwt":{
+                       "scopes":[
+                           "hello"
+                       ],
+                       "claims":{
+                           "aud":"customers",
+                           "iss":"https://hello.test.com/",
+                           "iat":"1587749276",
+                           "exp":"1587756476"
+                       }
+                   }
+               },
+               "accountId":"0123456789",
+               "stage":"$default",
+               "domainName":"hello.test.com",
+               "apiId":"pb5dg6g3rg",
+               "requestId":"LgLpnibOFiAEPCA=",
+               "http":{
+                   "path":"/hello",
+                   "userAgent":"Paw/3.1.10 (Macintosh; OS X/10.15.4) GCDHTTPRequest",
+                   "method":"GET",
+                   "protocol":"HTTP/1.1",
+                   "sourceIp":"91.64.117.86"
+               },
+               "time":"24/Apr/2020:17:47:41 +0000"
+           },
+           "isBase64Encoded":false,
+           "rawQueryString":"foo=bar",
+           "queryStringParameters":{
+               "foo":"bar"
+           },
+           "headers":{
+               "x-forwarded-proto":"https",
+               "x-forwarded-for":"91.64.117.86",
+               "x-forwarded-port":"443",
+               "authorization":"Bearer abc123",
+               "host":"hello.test.com",
+               "x-amzn-trace-id":"Root=1-5ea3263d-07c5d5ddfd0788bed7dad831",
+               "user-agent":"Paw/3.1.10 (Macintosh; OS X/10.15.4) GCDHTTPRequest",
+               "content-length":"0"
+           }
+       }
+       """
+    
+    // MARK: - Request -
+    // MARK: Decoding
+    func testRequestDecodingExampleGetRequest() {
+        let data = APIGatewayV2Tests.exampleGetEventBody.data(using: .utf8)!
+        var req: APIGatewayV2Request?
+        XCTAssertNoThrow(req = try JSONDecoder().decode(APIGatewayV2Request.self, from: data))
+        
+        XCTAssertEqual(req?.rawPath, "/hello")
+        XCTAssertEqual(req?.context.http.method, .GET)
+        XCTAssertEqual(req?.queryStringParameters?.count, 1)
+        XCTAssertEqual(req?.rawQueryString, "foo=bar")
+        XCTAssertEqual(req?.headers.count, 8)
+        XCTAssertNil(req?.body)
+    }
+    
+    static let exampleProxyEventBody = """
     {
         "resource": "/hello",
         "path": "/hello",
@@ -115,12 +186,12 @@ class APIGatewayV2Tests: XCTestCase {
     // MARK: - Request -
 
     // MARK: Decoding
-
-    func testRequestDecodingExampleGetRequest() {
-        let data = APIGatewayV2Tests.exampleGetEventBody.data(using: .utf8)!
-        var req: APIGatewayV2Request?
-        XCTAssertNoThrow(req = try JSONDecoder().decode(APIGatewayV2Request.self, from: data))
-
+    
+    func testRequestDecodingExampleProxyRequest() {
+        let data = APIGatewayV2Tests.exampleProxyEventBody.data(using: .utf8)!
+        var req: APIGatewayV2ProxyRequest?
+        XCTAssertNoThrow(req = try JSONDecoder().decode(APIGatewayV2ProxyRequest.self, from: data))
+        
         XCTAssertEqual(req?.path, "/hello")
         XCTAssertEqual(req?.context.httpMethod, .GET)
         XCTAssertEqual(req?.queryStringParameters?.count, 1)
